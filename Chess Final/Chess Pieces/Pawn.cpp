72722286c7f95c83a0a6066e2c7f30889ecf1547
Xenon <87145730+its-x3non@users.noreply.github.com>
2023-05-28 21:26:18 -0700

    Update Pawn.cpp

--- Chess Final/Chess Pieces/Pawn.cpp
+++ Chess Final/Chess Pieces/Pawn.cpp
@@ -1,15 +1,16 @@
 #include "Pawn.h"
-
 #include <iostream>
 
+// Constructor
 Pawn::Pawn(ChessBoard* board, Color color) : ChessPiece(board, color)
 {
-    wasMoved = false;
+    // wasMoved = false;
 }
 
-Pawn::~Pawn()
-{
-}
+// Destructor
+Pawn::~Pawn(){}
+
+// Returns a string representation of the pawn
 string Pawn::toString() const
 {
     if (color == BLACK)
@@ -18,11 +19,14 @@ string Pawn::toString() const
         return "\u2659";
 }
 
+// Returns a vector of legal moves for the pawn
 vector<string>* Pawn::legalMoves() const
 {
+    // Create a new vector to store legal moves
     vector<string>* res = new vector<string>();
     int c_row = row, c_col = column;
 
+    // Check if the pawn is black
     if (color == BLACK)
     {
         // Normal move
@@ -52,6 +56,7 @@ vector<string>* Pawn::legalMoves() const
                 res->push_back(ChessBoard::coordToString(c_row, c_col));
         }
     }
+    // Check if the pawn is white
     else if (color == WHITE) {
         // Normal move
         if (board->getPiece(Position(c_row + 1, c_col)) == NULL)
@@ -79,5 +84,6 @@ vector<string>* Pawn::legalMoves() const
                 res->push_back(ChessBoard::coordToString(c_row, c_col));
         }
     }
+    // Return the vector of legal moves
     return res;
 }
