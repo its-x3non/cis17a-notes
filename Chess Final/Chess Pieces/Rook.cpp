db86f81fb49464e35059c2ef6b04da0117b54006
Xenon <87145730+its-x3non@users.noreply.github.com>
2023-05-28 21:29:31 -0700

    Update Rook.cpp

--- Chess Final/Chess Pieces/Rook.cpp
+++ Chess Final/Chess Pieces/Rook.cpp
@@ -1,13 +1,12 @@
 #include "Rook.h"
 
-Rook::Rook(ChessBoard* board, Color color) : ChessPiece(board, color)
-{
-}
+// Constructor
+Rook::Rook(ChessBoard* board, Color color) : ChessPiece(board, color){}
 
-Rook::~Rook()
-{
-}
+// Destructor
+Rook::~Rook(){}
 
+// Returns a string representation of the Rook object.
 string Rook::toString() const
 {
     if (color == BLACK)
@@ -16,53 +15,61 @@ string Rook::toString() const
         return "\u2656";
 }
 
+// Returns a vector of strings representing the legal moves for the Rook object.
 vector<string>* Rook::legalMoves() const
 {
+    // Create a new vector to store the legal moves
     vector<string>* res = new vector<string>();
     int c_row, c_col;
 
-    for (c_row = row - 1; ChessBoard::isCorrect(c_row); c_row--) //push_back
+    // Check for legal moves in the upward direction
+    for (c_row = row - 1; ChessBoard::isCorrect(c_row); c_row--)
         if (board->getPiece(Position(c_row, column)) == NULL)
             res->push_back(ChessBoard::coordToString(c_row, column));
         else
             break;
-    //check for kill
+    // Check for legal moves to capture an opponent's piece
     if (ChessBoard::isCorrect(c_row))
         if (board->getPiece(Position(c_row, column)) != NULL)
             if (board->getPiece(Position(c_row, column))->getColor() != color)
                 res->push_back(ChessBoard::coordToString(c_row, column));
 
-    //moving throw current row right
-    for (c_row = row + 1; ChessBoard::isCorrect(c_row); c_row++) //push_back
+    // Check for legal moves in the downward direction
+    for (c_row = row + 1; ChessBoard::isCorrect(c_row); c_row++)
         if (board->getPiece(Position(c_row, column)) == NULL)
             res->push_back(ChessBoard::coordToString(c_row, column));
         else
             break;
-    // Chek for kill
+    // Check for legal moves to capture an opponent's piece
     if (ChessBoard::isCorrect(c_row))
         if (board->getPiece(Position(c_row, column)) != NULL)
             if (board->getPiece(Position(c_row, column))->getColor() != color)
                 res->push_back(ChessBoard::coordToString(c_row, column));
 
-    for (c_col = column - 1; ChessBoard::isCorrect(c_col); c_col--) //push_back
+    // Check for legal moves to the left
+    for (c_col = column - 1; ChessBoard::isCorrect(c_col); c_col--)
         if (board->getPiece(Position(row, c_col)) == NULL)
             res->push_back(ChessBoard::coordToString(row, c_col));
         else
             break;
+    // Check for legal moves to capture an opponent's piece
     if (ChessBoard::isCorrect(c_col))
         if (board->getPiece(Position(row, c_col)) != NULL)
             if (board->getPiece(Position(row, c_col))->getColor() != color)
                 res->push_back(ChessBoard::coordToString(row, c_col));
 
-    for (c_col = column + 1; ChessBoard::isCorrect(c_col); c_col++) //push_back
+    // Check for legal moves to the right
+    for (c_col = column + 1; ChessBoard::isCorrect(c_col); c_col++)
         if (board->getPiece(Position(row, c_col)) == NULL)
             res->push_back(ChessBoard::coordToString(row, c_col));
         else
             break;
+    // Check for legal moves to capture an opponent's piece
     if (ChessBoard::isCorrect(c_col))
         if (board->getPiece(Position(row, c_col)) != NULL)
             if (board->getPiece(Position(row, c_col))->getColor() != color)
                 res->push_back(ChessBoard::coordToString(row, c_col));
 
+    // Return the vector of legal moves
     return res;
 }
